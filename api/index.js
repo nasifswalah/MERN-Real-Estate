564d7c346556fc648b9c9e3fe3e9eddb79fbf6f2
nasifswalah <nasifswalah@gmail.com>
2024-05-13 20:20:59 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.listen('3000', () => {
-    console.log("Server running at port 3000");
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected DB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen("3000", () => {
+  console.log("Server running at port 3000");
+});
