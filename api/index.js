8707794979ddeeb39d6cf4099acc4021603bd00a
nasifswalah <nasifswalah@gmail.com>
2024-05-14 22:15:17 +0530

    Created a test Api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/userRoute.js"
 
 dotenv.config();
 const app = express();
@@ -17,3 +18,5 @@ mongoose
 app.listen("3000", () => {
   console.log("Server running at port 3000");
 });
+
+app.use('/api/user', userRoute)
