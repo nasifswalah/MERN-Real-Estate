03893285c4c626fdc8c7c3cdbdfc8110670436a9
nasifswalah <nasifswalah@gmail.com>
2024-05-17 18:36:59 +0530

    created update user Api route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO_URL)
