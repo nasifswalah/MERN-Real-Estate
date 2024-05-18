3827f0bdf4bb0e6745a12b2dfffefa3394fa55af
nasifswalah <nasifswalah@gmail.com>
2024-05-18 10:43:37 +0530

    created the listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
+import listingRoute from "./routes/listingRoute.js"
 
 dotenv.config();
 
@@ -27,6 +28,7 @@ app.listen("3000", () => {
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/listing', listingRoute);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
