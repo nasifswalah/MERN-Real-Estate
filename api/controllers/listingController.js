ac62f39a459d45580b2506d20389848432408721
nasifswalah <nasifswalah@gmail.com>
2024-05-19 13:44:02 +0530

    Created get user listings API route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -1,10 +1,26 @@
-import Listing from '../models/listingModel.js'
+import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
     try {
-      const listing = await Listing.create(req.body);
-      return res.status(201).json(listing);
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+      return;
     } catch (error) {
-      next(error);
+      next(error.message);
     }
-  };
+  } else {
+    next(errorHandler(401, "You cannot access other users listings"));
+  }
+};
+
